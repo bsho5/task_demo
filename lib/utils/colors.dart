1af92034814884958fec57b36cc8d4ea539f125d
bsho5 <boos05598@gmail.com>
2024-12-03 21:14:31 +0200

    added custom utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+  import 'package:flutter/material.dart';
+
+List<Color> appColors = [
+    const Color.fromRGBO(249, 189, 173, 1),
+    const Color.fromRGBO(250, 217, 109, 1),
+    const Color.fromRGBO(204, 184, 255, 1),
+    const Color.fromRGBO(176, 234, 253, 1),
+    const Color.fromRGBO(255, 157, 194, 1)
+  ];
